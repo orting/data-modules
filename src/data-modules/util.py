73e11ce640581f06a533879c1104d9771a26e135
Silas Ørting <silas@di.ku.dk>
2022-07-06 12:23:00 +0200

    Function for verifying sha256 hashes

---
+++ src/data-modules/util.py
@@ -0,0 +1,29 @@
+'''Utility functions for data modules'''
+# pylint: disable=trailing-whitespace
+import mmap
+import hashlib
+
+__all__ = [
+    'verify_sha256',
+]
+
+def verify_sha256(path, checksum):
+    '''Verify sha256 checksum of a file
+    Parameteters
+    -----------
+    path : str
+      Path to file to verify
+
+    checksum : str
+      Expected checksum as hexstring
+
+    Returns
+    -------
+    bool
+      True if checksum of file match `checksum`, False otherwise
+    '''
+    hasher = hashlib.sha256()
+    with open(path, 'r+b') as infile:
+        mmapped = mmap.mmap(infile.fileno(), 0)
+        hasher.update(mmapped)
+    return hasher.hexdigest() == checksum
